ec760c3f4eeaabb2e7955e7a3f142368ece884bd
Lamarkes <lamark12ricarte@gmail.com>
2026-03-16 22:36:17 -0300

    :sparkles: feat: marcar tarefas como DONE

--- src/config_tasks.py
+++ src/config_tasks.py
@@ -1,12 +1,9 @@
 import json
 from create_json_file import json_file_config
+from datetime import date
 
 
-tasks = {'task_name':{}}
-
-str_json = json.dumps(tasks, indent=4)
-
-def add_tasks():
+def add_tasks(tasks):
     json_file_config()
     try:
         with open('files/tasks.json', 'r', encoding="utf-8") as file:
@@ -19,3 +16,18 @@ def add_tasks():
     with open('files/tasks.json', 'w', encoding="utf-8") as json_file:
         json.dump(data, json_file, indent=4, ensure_ascii=False)
 
+
+def mark_a_task_as_done(id_task):
+
+    with open('files/tasks.json', 'r', encoding="utf-8") as file:
+        data = json.load(file)
+
+        for task in data['tasks']:
+            print(task['id'])
+            if task['id'] == id_task:
+                task['status'] = 'DONE'
+                task['updatedAt'] = str(date.today())
+                print("Task complete: ", task["title_task"])
+
+    with open('files/tasks.json', 'w', encoding="utf-8") as json_file:
+        json.dump(data, json_file, indent=4, ensure_ascii=False)
